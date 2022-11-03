ef5a03a8306bf35fce62c8c94d2abc67e47aeac1
cheeselicker100 <117392273+cheeselicker100@users.noreply.github.com>
2022-11-03 14:33:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Update Check
-function readTextFile(file, callback) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.overrideMimeType("application/json");
-  rawFile.open("GET", file, true);
-  rawFile.onreadystatechange = function() {
-    if (rawFile.readyState === 4 && rawFile.status == "200") {
-        callback(rawFile.responseText);
-     }
-  }
-  rawFile.send(null);
-}
-);
-
 // Main Div
 var devtools = document.createElement("div")
 devtools.setAttribute("class", "avodevtools")
