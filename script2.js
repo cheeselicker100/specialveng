5f60e5ba3933c81960fb7103df4746e5690bd569
cheeselicker100 <117392273+cheeselicker100@users.noreply.github.com>
2022-11-03 16:38:12 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -155,7 +155,7 @@ tic.setAttribute("onclick", "javascript:(function () {var script=document.create
 tic.setAttribute("class", "obtn")
 tic.innerText = "Ultimate Tic-Tac-Toe"
 var char = document.createElement("button")
-char.setAttribute("onclick", "javascript:(function () {var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/cheeselicker100/specialveng@main/extra.min.js';document.body.appendChild(script);avoclosedevtools()}())")
+char.setAttribute("onclick", "javascript:(function () {var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/cheeselicker100/specialveng@main/extra2.min.js';document.body.appendChild(script);avoclosedevtools()}())")
 char.setAttribute("class", "obtn")
 char.innerText = "Weird Characters"
 var git = document.createElement("button")
